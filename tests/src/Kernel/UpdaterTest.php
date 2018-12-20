3639e5f06a8b73f1d47fdf8c22f55393a60a3000
Mladen Todorovic <mladen.todorovic@burda.com>
2018-12-20 16:18:55 +0100

    Added testing for configuration import global action

--- tests/src/Kernel/UpdaterTest.php
+++ tests/src/Kernel/UpdaterTest.php
@@ -13,6 +13,25 @@ use Drupal\KernelTests\KernelTestBase;
  */
 class UpdaterTest extends KernelTestBase {
 
+  /**
+   * Config directory path.
+   *
+   * @var string
+   */
+  protected $configDir = '';
+
+  /**
+   * Module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler = NULL;
+
+  /**
+   * Following configurations will be manipulated during testing.
+   *
+   * @var string[]
+   */
   protected static $configSchemaCheckerExclusions = [
     'field.storage.node.body',
   ];
@@ -25,6 +44,7 @@ class UpdaterTest extends KernelTestBase {
     'text',
     'field',
     'node',
+    'tour',
   ];
 
   /**
@@ -37,7 +57,10 @@ class UpdaterTest extends KernelTestBase {
     return [
       '__global_actions' => [
         'install_modules' => [
-          'tour',
+          'help',
+        ],
+        'import_configs' => [
+          'tour.tour.tour-update-helper-test',
         ],
       ],
       'field.storage.node.body' => [
@@ -70,6 +93,61 @@ class UpdaterTest extends KernelTestBase {
   }
 
   /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->moduleHandler = \Drupal::moduleHandler();
+    $this->configDir = $this->moduleHandler->getModule('update_helper')->getPath() . '/config';
+
+    mkdir($this->configDir . '/install', 0755, TRUE);
+
+    // Prepare config file for testing of configuration import.
+    $tour_config = [
+      'id' => 'tour-update-helper-test',
+      'module' => 'update_helper',
+      'label' => 'Tour test Update Helper config import',
+      'langcode' => 'en',
+      'routes' => [
+        ['route_name' => 'update_helper.1'],
+      ],
+      'tips' => [
+        'tour-update-helper-test-1' => [
+          'id' => 'update-helper-test-1',
+          'plugin' => 'text',
+          'label' => 'Update Helper',
+          'body' => 'Update helper test tour.',
+          'weight' => 1,
+        ],
+      ],
+    ];
+
+    /** @var \Drupal\Core\Serialization\Yaml $yml_serializer */
+    $yml_serializer = \Drupal::service('serialization.yaml');
+    file_put_contents($this->configDir . '/install/tour.tour.tour-update-helper-test.yml', $yml_serializer->encode($tour_config));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function tearDown() {
+    $config_dir = $this->moduleHandler->getModule('update_helper')->getPath() . '/config';
+
+    // Remove import file.
+    unlink($config_dir . '/install/tour.tour.tour-update-helper-test.yml');
+    rmdir($config_dir . '/install');
+
+    // Remove configuration update definition.
+    unlink($config_dir . '/update/test_updater.yml');
+    rmdir($config_dir . '/update');
+
+    rmdir($config_dir);
+
+    parent::tearDown();
+  }
+
+  /**
    * @covers \Drupal\update_helper\Updater::executeUpdate
    */
   public function testExecuteUpdate() {
@@ -95,22 +173,28 @@ class UpdaterTest extends KernelTestBase {
     /** @var \Drupal\update_helper\Updater $update_helper */
     $update_helper = \Drupal::service('update_helper.updater');
 
-    /** @var \Drupal\update_helper\ConfigHandler $config_handler */
-    $config_handler = \Drupal::service('update_helper.config_handler');
-    $patch_file_path = $config_handler->getPatchFile('update_helper', 'test_updater', TRUE);
-
     /** @var \Drupal\Core\Serialization\Yaml $yml_serializer */
     $yml_serializer = \Drupal::service('serialization.yaml');
+
+    /** @var \Drupal\update_helper\ConfigHandler $config_handler */
+    $config_handler = \Drupal::service('update_helper.config_handler');
+
+    $patch_file_path = $config_handler->getPatchFile('update_helper', 'test_updater', TRUE);
     file_put_contents($patch_file_path, $yml_serializer->encode($this->getUpdateDefinition()));
 
-    /** @var \Drupal\Core\Extension\ModuleHandlerInterface $module_handler */
-    $module_handler = \Drupal::moduleHandler();
-    $this->assertFalse($module_handler->moduleExists('tour'), 'Module "tour" should not be installed.');
+    $this->assertFalse($this->moduleHandler->moduleExists('help'), 'Module "help" should not be installed.');
+
+    // Create some configuration file for tour, so that it can be imported.
+    $this->assertEquals(NULL, $config_factory->get('tour.tour.tour-update-helper-test')->get('id'), 'Tour configuration should not exist.');
+
+    // Ensure that configuration had new values.
+    $this->assertEquals('text', $config_factory->get('field.storage.node.body')->get('lost_config'));
 
     $update_helper->executeUpdate('update_helper', 'test_updater');
 
     $this->assertEquals($expected_config_data, $config_factory->get('field.storage.node.body')->get());
-    $this->assertTrue($module_handler->moduleExists('tour'), 'Module "tour" should be installed.');
+    $this->assertTrue($this->moduleHandler->moduleExists('help'), 'Module "help" should be installed.');
+    $this->assertEquals('tour-update-helper-test', $this->container->get('config.factory')->get('tour.tour.tour-update-helper-test')->get('id'), 'Tour configuration should exist.');
   }
 
 }
