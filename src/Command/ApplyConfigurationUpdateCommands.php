d387506f752b42a102d6ae54286b4af8f9639e62
Waleed Qadi <qadiwaleed@gmail.com>
2019-05-04 16:47:14 +0300

    Create a drush command to run updates

---
+++ src/Command/ApplyConfigurationUpdateCommands.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\update_helper\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\update_helper\Utility\CommandHelper;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class ApplyConfigurationUpdateCommand
+ *
+ * define drush commands for update_helper module
+ *
+ * @package Drupal\update_helper\Commands
+ */
+class ApplyConfigurationUpdateCommands extends DrushCommands {
+
+  /**
+   * command helper object (inspired by search API module)
+   *
+   * @var \Drupal\update_helper\Utility\CommandHelper
+   */
+  protected $commandHelper;
+
+  /**
+   * VarbaseUpdateHelperCommands constructor.
+   */
+  public function __construct() {
+    $this->commandHelper = new CommandHelper();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setLogger(LoggerInterface $logger) {
+    parent::setLogger($logger);
+    $this->commandHelper->setLogger($logger);
+  }
+
+  /**
+   * applying an update hook (function) from module install file
+   * Apply updates by invoking the related update hooks.
+   *
+   * @param string $module
+   * @param string $update_hook
+   * @param array $options
+   * @option force
+   *
+   * @command update_helper:apply-update
+   * @aliases uhau
+   */
+  public function apply_update ($module = '', $update_hook = '', $options = ['force' => FALSE]) {
+    $force = $options['force'];
+    $this->commandHelper->apply_update($module, $update_hook, $force);
+  }
+}
---
+++ src/Utility/CommandHelper.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\update_helper\Utility;
+
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+
+class CommandHelper implements LoggerAwareInterface {
+
+  use LoggerAwareTrait;
+
+  public function __construct() {
+
+  }
+
+  /**
+   * applying an (optional) update hook (function) from module install file
+   *
+   * @param string $module - drupal module name
+   * @param string $update_hook - name of update_hook to apply
+   * @param bool $force - force the update
+   */
+  public function apply_update($module = '', $update_hook = '', $force = FALSE) {
+    if (!$update_hook || !$module) {
+      $this->logger->info(dt('Please provide a module name and an update hook. Example: drush varbase-up <module> <update_hook>'));
+      return;
+    }
+
+    $updateHelper = \Drupal::service('update_helper.updater');
+    $updateHelper->executeUpdate($module, $updateName, $force);
+    return $updateHelper->logger()->output();
+  }
+
+}
